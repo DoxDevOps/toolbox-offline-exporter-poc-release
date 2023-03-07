501900190589b69d71068ed934c7482b0ddf818c
dominickasanga <dominickasanga@gmail.com>
2023-03-07 10:52:27 +0200

    modified:   install-node-npm-offline.sh 	deleted:    npm-8.1.0.tgz

--- install-node-npm-offline.sh
+++ install-node-npm-offline.sh
@@ -5,12 +5,5 @@ sudo chown -R root:root /usr/local/node-v14.18.0-linux-x64
 sudo ln -s /usr/local/node-v14.18.0-linux-x64/bin/node /usr/bin/node
 sudo ln -s /usr/local/node-v14.18.0-linux-x64/bin/npm /usr/bin/npm
 
-# Step 2: Install npm from the downloaded npm tarball
-mkdir -p ~/.npm-global
-tar -xvf npm-8.1.0.tgz -C ~/.npm-global --strip-components=1
-echo 'export PATH=~/.npm-global/bin:$PATH' >> ~/.bashrc
-source ~/.bashrc
-npm config set prefix '~/.npm-global'
-
-# Step 3: Install PM2
+# Step 2: Install PM2
 npm install pm2-5.2.2.tgz -g --verbose --no-progress

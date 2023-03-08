e5b42820703fa351154bd0c2e98548b6508722ba
dominickasanga <dominickasanga@gmail.com>
2023-03-08 07:05:20 +0200

    modified:   reconfig.sh 	modified:   setup.sh

--- reconfig.sh
+++ reconfig.sh
@@ -1,3 +1,15 @@
 cd node_modules/toolbox-offline-exporter-poc-nodejs
+echo '##############################################################'
+echo '#                   ToolBox Configuration                    #'
+echo '#                                                            #'
+echo '##############################################################'
 node setup.js
 pm2 start index.js --name toolbox
+
+
+echo '######################################################################'
+echo ''
+echo 'ToolBox listens on port 6070'
+echo 'using the server ip address'
+echo 'for example, the  URL will be  http:127.0.0.1//:6070'
+echo 'replace 127.0.0.1 with the server ip address and visit/open the URL in the browser'
--- setup.sh
+++ setup.sh
@@ -16,5 +16,17 @@ npm install pm2-5.2.2.tgz -g --verbose --no-progress
 npm install toolbox-offline-exporter-poc-nodejs-1.0.0.tgz --verbose --no-progress
 cd node_modules/toolbox-offline-exporter-poc-nodejs
 npm install --verbose --no-progress
+echo '##############################################################'
+echo '#                   ToolBox Configuration                    #'
+echo '#                                                            #'
+echo '##############################################################'
 node setup.js
 pm2 start index.js --name toolbox
+
+
+echo '######################################################################'
+echo ''
+echo 'ToolBox listens on port 6070'
+echo 'using the server ip address'
+echo 'for example, the  URL will be  http:127.0.0.1//:6070'
+echo 'replace 127.0.0.1 with the server ip address and open in the browser'

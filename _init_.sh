6b9f4aa800513f6cbf06d978ac49c5f65a2115cd
dominickasanga <dominickasanga@gmail.com>
2023-04-20 19:02:33 +0200

    modified:   _init_.sh

--- _init_.sh
+++ _init_.sh
@@ -1,3 +1,3 @@
 cd /var/www/toolbox-offline-exporter-poc-release
 npx pm2 delete toolbox
-npx pm2 start index.js -f --name toolbox --cwd toolbox-offline-exporter-poc-nodejs
+npx pm2 start index.js -f --name toolbox --cwd toolbox-offline-exporter-poc-nodejs --interpreter /usr/local/node-v14.18.0-linux-x64/bin/node
